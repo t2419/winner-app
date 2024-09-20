283a268aff890da5e9ec036a2c1727ace7f1c4fb
zz-tiger <ken2423236821@gmail.com>
2024-09-20 23:30:45 +0800

    new

---
+++ container-uat/src/main/java/com/winner/OpreateUtil.java
@@ -0,0 +1,18 @@
+package com.winner;
+
+import java.lang.reflect.Proxy;
+
+public class OpreateUtil {
+
+    public static void main(String[] args) {
+
+        int ten = 10;
+        System.out.println(ten ^ 0b0110);
+        System.out.println(ten | 0b0110);
+        System.out.println(0b100);
+
+//        Proxy
+
+    }
+
+}
